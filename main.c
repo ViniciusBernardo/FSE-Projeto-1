0f2bcf977cc2f5672ad3b8d8e6985559155d5e87
Vinicius Lima <vfbdelima@gmail.com>
2020-10-11 16:57:22 -0300

    Added lcd module
    Signed-off-by: Vinicius Lima <vfbdelima@gmail.com>

--- main.c
+++ main.c
@@ -1,12 +1,20 @@
 #include "sensor_bme280/sensor_bme280.c"
+#include "lcd_16x2/i2c_lcd.c"
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <time.h>
 
 int main(int argc, char ** argv){
+    float external_temperature;
     while(1){
-        printf("Temperatura Externa: %.2f °C\n", get_external_temperature("/dev/i2c-1"));
+        external_temperature = get_external_temperature("/dev/i2c-1");
+        printf("Temperatura Externa: %.2f °C\n", external_temperature);
+
+        char line_1[16];
+        sprintf(line_1, "TE: %.2f", external_temperature); 
+        
+        writeOnLCD(line_1, "");
     }
     return 0;
 }
