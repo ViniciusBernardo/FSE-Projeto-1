54b404a4642b6e3c78ed28248f22a9e6900aefea
Vinicius Lima <vfbdelima@gmail.com>
2020-10-12 10:20:11 -0300

    Added uart communication asking TI and TR
    Signed-off-by: Vinicius Lima <vfbdelima@gmail.com>

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include "sensor_bme280/sensor_bme280.c"
 #include "lcd_16x2/lcd_display.c"
+#include "uart/uart.c"
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -7,14 +8,22 @@
 
 int main(int argc, char ** argv){
     float external_temperature;
+    float reference_temperature;
+    float internal_temperature;
     while(1){
         external_temperature = get_external_temperature("/dev/i2c-1");
+        reference_temperature = get_temperature("TR");
+        internal_temperature = get_temperature("TI");
         printf("Temperatura Externa: %.2f °C\n", external_temperature);
+        printf("Temperatura De Referência: %.2f °C\n", reference_temperature);
+        printf("Temperatura Interna: %.2f °C\n", internal_temperature);
 
         char line_1[16];
-        sprintf(line_1, "TE: %.2f", external_temperature); 
+        char line_2[16];
+        sprintf(line_1, "TR: %.2f TE: %.1f", reference_temperature, external_temperature);
+        sprintf(line_2, "TI: %.2f", internal_temperature);
         
-        showLines(line_1, "");
+        showLines(line_1, line_2);
     }
     return 0;
 }
---
+++ uart/uart.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <termios.h>
+
+
+float get_temperature(const char *temperature_name) { // temperature_name = TI || TR
+    int uart0_filestream = -1;
+    float temperature = -40;
+
+    uart0_filestream = open("/dev/serial0", O_RDWR | O_NOCTTY | O_NDELAY);
+    if(uart0_filestream == -1) {
+        prinf("Erro - Porta serial não pode ser aberta. Confirme se não está sendo usada por outra aplicação.\n");
+        return temperature;
+    }
+
+    struct termios options;
+	tcgetattr(uart0_filestream, &options);
+	options.c_cflag = B115200 | CS8 | CLOCAL | CREAD;		//<Set baud rate
+	options.c_iflag = IGNPAR;
+	options.c_oflag = 0;
+	options.c_lflag = 0;
+	tcflush(uart0_filestream, TCIFLUSH);
+	tcsetattr(uart0_filestream, TCSANOW, &options);
+
+	unsigned char tx_buffer[20];
+	unsigned char *p_tx_buffer;
+
+    if(temperature_name == "TI") {
+        p_tx_buffer = &tx_buffer[0];
+        *p_tx_buffer++ = 0xA1;
+        *p_tx_buffer++ = 1;
+        *p_tx_buffer++ = 3;
+        *p_tx_buffer++ = 3;
+        *p_tx_buffer++ = 1;
+    } else if (temperature_name == "TR") {
+        p_tx_buffer = &tx_buffer[0];
+        *p_tx_buffer++ = 0xA2;
+        *p_tx_buffer++ = 1;
+        *p_tx_buffer++ = 3;
+        *p_tx_buffer++ = 3;
+        *p_tx_buffer++ = 1;
+    } else {
+        printf("Invalid temperature_name!! (%s)\n", temperature_name);
+        return temperature;
+    }
+	
+	if (uart0_filestream != -1) {
+		int count = write(uart0_filestream, &tx_buffer[0], (p_tx_buffer - &tx_buffer[0]));		//Filestream, bytes to write, number of bytes to write
+		if (count < 0){
+			printf("UART TX error\n");
+		}
+	}
+
+    sleep(1);
+
+    //----- CHECK FOR ANY RX BYTES -----
+	if (uart0_filestream != -1) {
+		// Read up to 255 characters from the port if they are there
+		unsigned char rx_buffer[256];
+		int rx_length = read(uart0_filestream, (void*)rx_buffer, 255);		//Filestream, buffer to store in, number of bytes to read (max)
+		if (rx_length < 0) {
+            printf("Erro na leitura.\n");
+		} else if (rx_length == 0) {
+            printf("Nenhum dado disponível.\n");
+		} else {
+			//Bytes received
+			rx_buffer[rx_length] = '\0';
+            temperature = (float)rx_buffer;
+			printf("%i bytes read : %.2f\n", rx_length, temperature);
+		}
+	}
+
+    close(uart0_filestream);
+    return temperature;
+}
